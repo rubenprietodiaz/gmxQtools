89a1b25ed4402c14ebbbd5209064a8386dd40cb2
rubenprietodiaz <ruben.prieto@usc.es>
2024-04-25 22:22:16 +0200

    Some small changes
    Add description of functions and to-do for setup_sim.py

--- setup_sim.py
+++ setup_sim.py
@@ -11,10 +11,8 @@ def parse_arguments():
     parser.add_argument("-f", "--fep", help="Directory of the complex to prepare FEP files. Omit if you don't want to prepare FEP files.")
     return parser.parse_args()
 
-# Parse arguments
-args = parse_arguments()
-
 def copy_files_in_directory(directory, destination_folder):
+    """Iterate over the folders within the current directory and call the copy_files function."""
     # Iterate over the folders within the current directory
     for root, dirs, files in os.walk(directory):
         for dir in dirs:
@@ -27,6 +25,7 @@ def copy_files_in_directory(directory, destination_folder):
 
 
 def copy_files(folder, destination_folder):
+    """Copy the required files for MD simulations to the destination folder."""
     # Check the existence of each required file
     if all(os.path.isfile(os.path.join(folder, file)) for file in ["prod.mdp", "topol.top", "index.ndx","topol.tpr"]) and os.path.exists(os.path.join(folder, "finalOutput", "confout.gro")):
         # Extract the name of the current folder
@@ -62,6 +61,7 @@ def copy_files(folder, destination_folder):
         return False
 
 def create_submit_script(destination_folder):
+    """Create the submit.sh script to submit the MD simulations."""
     submit_script_content = """#!/bin/bash
 
 start_dir=$(pwd)
@@ -78,6 +78,7 @@ done
         submit_script_file.write(submit_script_content)
 
 def create_run_md_script(destination_folder):
+    """Create the run_md.sh script to run the MD simulations in all the subfolders."""
     if args.cluster == "CSB":
         run_md_script_content = f"""#!/bin/bash
 #SBATCH -N 1
@@ -124,6 +125,7 @@ srun gmx mdrun -s topol_prod.tpr -o traj.trr -e ener.edr -c final.gro -g product
     os.chmod(run_md_script_path, 0o755)
 
 def modify_simulation_time(destination_folder):
+    """Modify the simulation time in the prod.mdp file."""
     simulation_time_ns = args.simulation_time
     nsteps = simulation_time_ns * 500000  # 1 ns = 500000 steps
 
@@ -139,17 +141,18 @@ def modify_simulation_time(destination_folder):
                 prod_mdp_file.write(line)
 
 def prepare_fep_files(complex_directory, destination_folder_fep):
-    # Asegurarnos de que la ruta al directorio complejo es absoluta y existe
+    """Prepare the FEP files for the complex in the specified directory."""
+    # Check if the complex directory exists
     complex_directory = os.path.abspath(complex_directory)
     if not os.path.isdir(complex_directory):
         print(f"Error: The specified complex directory {complex_directory} does not exist.")
         return
 
-    # Definir los directorios correctamente
+    # Check if the destination folder for FEP files exists
     eqProd_directory = os.path.join(complex_directory, "eqProd")
     confout_gro = os.path.join(eqProd_directory, "confout200.gro")
 
-    # Verificar si confout200.gro existe antes de continuar
+    # Check if confout200.gro exists
     if not os.path.isfile(confout_gro):
         print(f"Error: confout200.gro not found in {eqProd_directory}.")
         return
@@ -168,6 +171,7 @@ def prepare_fep_files(complex_directory, destination_folder_fep):
     print(f"File confout_fep.pdb created successfully in {destination_folder_fep}.")
 
 def process_water_molecules(input_pdb, output_pdb):
+    """Process water molecules in the input PDB file. Generate a new PDB file with only water molecules around the polymer."""
     pymol_script = f"""
 load {input_pdb}
 select water, solvent within 10 of polymer
@@ -181,12 +185,15 @@ save {output_pdb}, water_3
     os.remove("get_waters.pml")
 
 def rename_and_remove_elements(pdb_file):
+    """Rename and remove elements in the PDB file."""
     subprocess.run(["grep", "'SOL'", pdb_file, ">", "temp.pdb"])
     shutil.move("temp.pdb", pdb_file.replace("confout_water", "confout"))
     subprocess.run(["sed", "-i", "'s/SOL/HOH/g'", pdb_file])
     subprocess.run(["sed", "-i", "'s/OW/O /g'", pdb_file])
     subprocess.run(["sed", "-i", "'s/HW1/H1 /g'", pdb_file])
     subprocess.run(["sed", "-i", "'s/HW2/H2 /g'", pdb_file])
+    subprocess.run(["sed", "-i", "'s/O2 /OXT/g'", pdb_file]) # Check this modifications
+    
 
 def clean_pdb(original_pdb, ligand):
     temp_file = original_pdb.replace('.pdb', '_temp.pdb')
@@ -205,12 +212,22 @@ def clean_pdb(original_pdb, ligand):
     shutil.move(temp_file, original_pdb)
     print(f"Removed unwanted lines from {original_pdb} and saved ligand to {ligand_file}.")
     
-def add_water_to_pdb(pdb_file, water_pdb):
+def merge_water_and_pdb(pdb_file, water_pdb):
+    """Merge the water molecules and the PDB file."""
     with open(water_pdb, 'r') as water_file, open(pdb_file, 'a') as file:
         file.writelines(water_file.readlines())
 
+def remove_last_two_characters(original_file, modified_file):
+    """Remove the last two characters from each line in the original file and save the modified file, for better visualization."""
+    with open(original_file, 'r') as file_in, open(modified_file, 'w') as file_out:
+        for line in file_in:
+            if line.strip():
+                file_out.write(line[:-2] + '\n')
 
-# Define the destination folder
+# RUN
+args = parse_arguments()
+
+# Define the destination folder // TO DO: solve problem when trying to create only FEP from --full_relax false
 destination_folder = "md_input_files"
 
 # Call the function to start copying files in the current directory
@@ -223,14 +240,18 @@ if args.fep:
     prepare_fep_files(args.fep, destination_folder_fep)
 
     # Define paths to the PDB files
-    input_pdb = os.path.join(destination_folder_fep, "confout_fep.pdb")
-    output_pdb = os.path.join(destination_folder_fep, "confout_water.pdb")
+    confout_pdb = os.path.join(destination_folder_fep, "confout_fep.pdb")
+    water_pdb = os.path.join(destination_folder_fep, "confout_water.pdb")
 
     # Process water molecules
-    process_water_molecules(input_pdb, output_pdb)
-    clean_pdb(input_pdb, "L01")
-    add_water_to_pdb(input_pdb, output_pdb)
-    os.remove(output_pdb) # Remove the temporary file of waters
+    process_water_molecules(confout_pdb, water_pdb)
+    clean_pdb(confout_pdb, "L01")
+    merge_water_and_pdb(confout_pdb, water_pdb)
+    
+    # TO DO: Rename and remove elements
+    # Rename files with rename_and_remove_elements and remove_last_two_characters
+    
+    os.remove(water_pdb) # Remove the temporary file of waters
 
 else:
     print("No complex directory specified for FEP preparation.")
