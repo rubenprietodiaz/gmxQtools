f79d9bc52ac149685d02a0f9f3dd58bdefd17a97
rubenprietodiaz <ruben.prieto@usc.es>
2024-06-22 14:46:45 +0200

    Update rmsd_smarts.py
    SMILES and inversion arguments added

--- 2.analysis/rmsd_smarts.py
+++ 2.analysis/rmsd_smarts.py
@@ -5,14 +5,17 @@ import pandas as pd # type: ignore
 import numpy as np  # type: ignore
 from rdkit import Chem # type: ignore
 from rdkit.Chem import AllChem # type: ignore
+from rdkit.Chem import rdFMCS # type: ignore
 from collections import defaultdict
 
 # Argument parser setup and input parameters
 parser = argparse.ArgumentParser(description="Calculate RMSD of a ligand in trajectory files.")
-parser.add_argument('-p', '--pdb_file', type=str, default='pymol/start.pdb', help='Path to the PDB file. Default is pymol/start.pdb')
+parser.add_argument('-p', '--pdb_file', type=str, default='pymol/start.pdb', help='Path to the PDB file. Default is pymol/start.pdb') # Change to finalOutput/start.pdb
 parser.add_argument('-s', '--smarts_pattern', type=str, help='SMARTS pattern. If not provided, analyze the whole ligand')
+parser.add_argument('-S', '--smiles', type=str, help='SMILES code. If provided, calculate maximum common substructure (MCS) with the ligand')
+parser.add_argument('-i', '--inverse', action='store_true', help='Calculate RMSD for atoms not matching the SMARTS pattern')
 parser.add_argument('-l', '--ligand_name', type=str, default='L01', help='Name of the ligand. Default is L01')
-parser.add_argument('-t', '--traj_file', type=str, default='pymol/traj_prod_pymol.xtc', help='Path to the trajectory file. Default is pymol/traj_prod_pymol.xtc')
+parser.add_argument('-t', '--traj_file', type=str, default='pymol/traj_prod_pymol.xtc', help='Path to the trajectory file. Default is pymol/traj_prod_pymol.xtc') # Change to finalOutput/traj_prod_pymol.xtc
 parser.add_argument('-o', '--output_filename_rmsd', type=str, default='rmsd_stat.txt', help='Output filename for RMSD results. Default is rmsd_stat.txt')
 parser.add_argument('-f', '--reference_frame', type=int, default=0, help='Frame to use as reference for RMSD calculation. Default is 0')
 
@@ -20,10 +23,19 @@ args = parser.parse_args()
 
 pdb_file = args.pdb_file
 smarts_pattern = args.smarts_pattern
+smiles = args.smiles
+inverse = args.inverse
 ligand_name = args.ligand_name
 traj_file = args.traj_file
 output_filename_rmsd = args.output_filename_rmsd
 reference_frame = args.reference_frame
+
+# Check for conflicting arguments
+if inverse and not smarts_pattern and not smiles:
+    print("Inverse flag provided without SMARTS pattern or SMILES code. Ignoring.")
+
+if smarts_pattern and smiles:
+    raise ValueError("Cannot provide both SMARTS pattern and SMILES code. Please choose one.")
 
 # Definitions
 def extract_ligand(pdb_file, ligand_name, output_file):
@@ -41,21 +53,33 @@ def smarts_match(smarts, ligand_pdb):
     mol_from_smarts = Chem.MolFromSmarts(smarts)
     return mol_from_pdb.GetSubstructMatches(mol_from_smarts) # List of lists with atom numbers
 
-def return_atom_numbers(smarts, ligand_pdb):
-    '''Returns the atom numbers in a ligand PDB file that match the SMARTS pattern.'''
-    rdkit_numbers = smarts_match(smarts, ligand_pdb)[0] # Get the first list of atom numbers
+def mcs_match(smiles, ligand_pdb):
+    '''Matches the Maximum Common Substructure (MCS) between a SMILES and a ligand PDB file.'''
+    mol_from_pdb = Chem.MolFromPDBFile(ligand_pdb)
+    mol_from_smiles = Chem.MolFromSmiles(smiles)
+    mcs_result = rdFMCS.FindMCS([mol_from_pdb, mol_from_smiles])
+    mcs_smarts = mcs_result.smartsString
+    mol_from_mcs_smarts = Chem.MolFromSmarts(mcs_smarts)
+    return mol_from_pdb.GetSubstructMatches(mol_from_mcs_smarts) # List of lists with atom numbers
+
+def return_atom_numbers(atom_matches, ligand_pdb):
+    '''Returns the atom numbers in a ligand PDB file from atom matches.'''
     with open(ligand_pdb, 'r') as file:
         lines = file.readlines()
     atom_numbers = []
-    for number in rdkit_numbers:
-        atom_numbers.append((int(lines[number].split()[1])) - 1) # Get the atom number from the PDB file | -1 to match the 0-based indexing of MDTraj
+    for match in atom_matches[0]: # Get the first list of atom numbers
+        atom_numbers.append((int(lines[match].split()[1])) - 1) # Get the atom number from the PDB file | -1 to match the 0-based indexing of MDTraj
     return atom_numbers
 
 # Run the script
 if smarts_pattern:
-    print(f'This script will calculate the RMSD of the ligand {ligand_name} in the trajectory files using the SMARTS pattern {smarts_pattern}.')
+    action = 'inverse ' if inverse else ''
+    print(f'This script will calculate the RMSD of the ligand {ligand_name} in the trajectory files using the {action}SMARTS pattern provided, with {reference_frame} being the reference frame.')
+elif smiles:
+    action = 'inverse' if inverse else ''
+    print(f'This script will calculate the RMSD of the ligand {ligand_name} in the trajectory files using the {action}MCS with SMILES provided, with {reference_frame} being the reference frame.')
 else:
-    print(f'This script will calculate the RMSD of the ligand {ligand_name} in the trajectory files.')
+    print(f'This script will calculate the RMSD of the ligand {ligand_name} in the trajectory files, with {reference_frame} being the reference frame.')
 
 results_rmsd = pd.DataFrame(columns=['Ligand', 'Mean_RMSD', 'Std_RMSD'])
 rmsd_data = defaultdict(list) # Dictionary to store RMSD data for each group
@@ -82,9 +106,20 @@ for subdir in os.listdir('.'):
     if smarts_pattern:
         ligand_pdb = f'{ligand_name}.pdb'
         extract_ligand(pdb_file_path, ligand_name, ligand_pdb) # Extract ligand from the trajectory
-        lig_atom_numbers = return_atom_numbers(smarts_pattern, ligand_pdb) # List of atom numbers in the ligand PDB file that match the SMARTS pattern
+        matching_atom_numbers = return_atom_numbers(smarts_match(smarts_pattern, ligand_pdb), ligand_pdb) # List of atom numbers in the ligand PDB file that match the SMARTS pattern
+    elif smiles:
+        ligand_pdb = f'{ligand_name}.pdb'
+        extract_ligand(pdb_file_path, ligand_name, ligand_pdb) # Extract ligand from the trajectory
+        matching_atom_numbers = return_atom_numbers(mcs_match(smiles, ligand_pdb), ligand_pdb) # List of atom numbers in the ligand PDB file that match the MCS with SMILES
     else:
-        lig_atom_numbers = traj.top.select(f'resname {ligand_name}')
+        matching_atom_numbers = traj.top.select(f'resname {ligand_name}')
+
+    if inverse and (smarts_pattern or smiles):
+        all_ligand_atoms = set(traj.top.select(f'resname {ligand_name}'))
+        non_matching_atom_numbers = list(all_ligand_atoms - set(matching_atom_numbers))
+        lig_atom_numbers = non_matching_atom_numbers
+    else:
+        lig_atom_numbers = matching_atom_numbers
 
     if len(lig_atom_numbers) == 0:
         print(f'No atoms found for ligand {ligand_name} in {subdir}.')
